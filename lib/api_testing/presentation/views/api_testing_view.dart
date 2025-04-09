bed46b9fd143360705fb746a935e9c1e1a67c0ba
AhmedOmran22 <mamdouhanahamada@gmail.com>
2025-04-09 07:18:50 +0200

    add Icon in floating action button

--- lib/api_testing/presentation/views/api_testing_view.dart
+++ lib/api_testing/presentation/views/api_testing_view.dart
@@ -21,6 +21,7 @@ class ApiTestingView extends StatelessWidget {
         floatingActionButton: Builder(
           builder: (context) {
             return FloatingActionButton(
+              child: const Icon(Icons.get_app, size: 32, color: Colors.black),
               onPressed: () {
                 context.read<ApiProductCubit>().getProducts();
               },
